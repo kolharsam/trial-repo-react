a3d8552f7ac55572c905a661cfa63c8f0e930402
Sam K <kolhar730@gmail.com>
2019-03-25 20:14:37 +0530

    new deployment site and added build

---
+++ build/precache-manifest.72a69ca632e9c94f8a020aab57586bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af2cde38bf95eb82a1a4",
+    "url": "/static/js/main.1b47ce7f.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "af2cde38bf95eb82a1a4",
+    "url": "/static/css/main.9d344b6e.chunk.css"
+  },
+  {
+    "revision": "bf6191ea5998535ff734b92115cf4df5",
+    "url": "/index.html"
+  }
+];
